97f8c8930f689f336c9da1fd01227ece3da2b96a
Phryxia <xahhaepica@gmail.com>
2024-05-08 14:59:07 +0900

    Apply `LabelPair` to `TagCheckboxes`

--- src/components/shared/TagCheckboxes.tsx
+++ src/components/shared/TagCheckboxes.tsx
@@ -1,36 +1,40 @@
 import classnames from 'classnames/bind'
 import styles from './TagCheckboxes.module.css'
-import { useState } from 'react'
+import { useLayoutEffect, useState } from 'react'
 import { ToggleButton } from './ToggleButton'
+import type { LabelPair } from '../../model/types'
 
 const cx = classnames.bind(styles)
 
 interface Props<T> {
-  label: string[]
-  domain: T[]
+  entries: LabelPair<T>[]
   selectedValues: T[]
   onChange(selectedValues: T[]): void
 }
 
-export function TagCheckboxes<T>({ label, domain, selectedValues, onChange }: Props<T>) {
+export function TagCheckboxes<T>({ entries, selectedValues, onChange }: Props<T>) {
   const [isChecked, setIsChecked] = useState(
-    domain.map((entry) => selectedValues.includes(entry)),
+    entries.map(({ value }) => selectedValues.includes(value)),
   )
 
   function createHandleChange(index: number) {
     return (newValue: boolean) => {
       const newIsChecked = isChecked.map((value, i) => (i === index ? newValue : value))
       setIsChecked(newIsChecked)
-      onChange(domain.filter((_, i) => newIsChecked[i]))
+      onChange(entries.filter((_, i) => newIsChecked[i]).map(({ value }) => value))
     }
   }
 
+  useLayoutEffect(() => {
+    setIsChecked(entries.map(({ value }) => selectedValues.includes(value)))
+  }, [selectedValues])
+
   return (
     <ul className={cx('tags')}>
-      {domain.map((_, i) => (
+      {entries.map(({ label }, i) => (
         <ToggleButton
-          key={label[i]}
-          label={label[i]}
+          key={label}
+          label={label}
           value={isChecked[i]}
           onChange={createHandleChange(i)}
         />
